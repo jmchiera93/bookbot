8ddd2f17cd55295665b6dad14fa422832006c26a
jmchiera93 <jchiera93@gmail.com>
2024-04-19 17:14:11 -0400

    added get_char_count() function to main script. This function counts each time a character appears in the book text and returns a dictionary

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 def main():
-    book_path = "books/frankenstein.txt"
-    print(read_book(book_path))
+    book_file_path = "books/frankenstein.txt"
+    book_text = get_book_contents(book_file_path)
+    book_word_count = get_word_count(book_text)
+    book_char_count = get_char_count(book_text.lower())
+    # print(book_text)
+    print(f"There are {book_word_count} words in the book.")
+    print(book_char_count)
 
 
-def read_book(file_path):
-    with open(file_path) as f:
+def get_book_contents(book_file_path):
+    with open(book_file_path) as f:
         return f.read()
+
+
+def get_word_count(book_text):
+    return len(book_text.split())
+
+
+def get_char_count(book_text):
+    book_text_list = book_text.split()
+    char_count_dict = {}
+    for word in book_text_list:
+        for char in word:
+            if char not in char_count_dict.keys():
+                char_count_dict[char] = int(1)
+            else:
+                char_count_dict[char] += 1
+    return char_count_dict
 
 
 if __name__ == "__main__":
