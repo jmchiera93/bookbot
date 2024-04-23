4781223d361bf13fb5703ddc359d56345e716846
jmchiera93 <jchiera93@gmail.com>
2024-04-23 10:46:53 -0400

    Ability to print a report added. print_report(), char_dict_to_list() and sort_on() functions added

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ def main():
     book_file_path = "books/frankenstein.txt"
     book_text = get_book_contents(book_file_path)
     book_word_count = get_word_count(book_text)
-    book_char_count = get_char_count(book_text)
-    # print(book_text)
-    print(f"There are {book_word_count} words in the book.")
-    print(book_char_count)
+    char_count_dict = get_char_count(book_text)
+    char_count_list = char_dict_to_list(char_count_dict)
+    print_report(book_file_path, book_word_count, char_count_list)
 
 
 def get_book_contents(book_file_path):
@@ -21,11 +20,32 @@ def get_char_count(book_text):
     book_text_lower = book_text.lower()
     char_count_dict = {}
     for char in book_text_lower:
-        if char not in char_count_dict:
-            char_count_dict[char] = 1
-        else:
+        if char in char_count_dict:
             char_count_dict[char] += 1
+        else:
+            char_count_dict[char] = 1
     return char_count_dict
+
+
+def print_report(book_file_path, book_word_count, char_count_list):
+    print(f"--- Begin report of {book_file_path} ---")
+    print(f"{book_word_count} words found in the document\n")
+    for i in char_count_list:
+        print(f"The '{i["char"]}' character was found {i["total"]} times")
+    print("--- End report ---")
+
+
+def char_dict_to_list(char_count_dict):
+    char_list = []
+    for k, v in char_count_dict.items():
+        if k.isalpha():
+            char_list.append({"char": k, "total": v})
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
+
+def sort_on(dict):
+    return dict["total"]
 
 
 if __name__ == "__main__":
